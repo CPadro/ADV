f8e2d5b569b42ae89413b09cadea34c5bfad42f5
Cristina Padró <34605397+SilverArrow23@users.noreply.github.com>
2021-03-26 20:44:53 +0100

    i

---
+++ Assets/00-Lesson5/Scripts/Collision.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    Animator anim;
+    private void OnTriggerEnter(Collider collision) 
+    {
+        anim = collision.gameObject.GetComponent<Animator>();
+        anim.SetBool("Kicked", !(anim.GetBool("Kicked")));
+    }
+}
