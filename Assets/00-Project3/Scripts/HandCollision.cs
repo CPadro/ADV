49b7051f92b03fbb907c997c1c970923ecba2191
Cristina Padró <34605397+SilverArrow23@users.noreply.github.com>
2021-04-14 15:04:50 +0200

    Almost all animations and sfx are done
    For some reason close particles and sfx not working

--- Assets/00-Project3/Scripts/HandCollision.cs
+++ Assets/00-Project3/Scripts/HandCollision.cs
@@ -6,10 +6,24 @@ public class HandCollision : MonoBehaviour
 {
     [SerializeField] Animator map;
     [SerializeField] Animator lid;
+    [SerializeField] AudioSource shimmer;
+    [SerializeField] AudioSource open;
+    [SerializeField] AudioSource close;
     private void OnTriggerEnter(Collider collision) 
     {
         lid = collision.gameObject.GetComponent<Animator>();
         map.SetBool("lidOpened", !(map.GetBool("lidOpened")));
         lid.SetBool("touchedLid", !(lid.GetBool("touchedLid")));
+        if(map.GetBool("lidOpened")) 
+        {
+            shimmer.Play();
+            open.Play();
+            close.Pause();
+        }
+        else {
+            shimmer.Pause();
+            close.Play();
+            open.Pause();
+        }
     }
 }
