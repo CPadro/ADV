a3f696fc840778c7e3d66ac7ae6dfd798e7aff30
Cristina Padró <34605397+SilverArrow23@users.noreply.github.com>
2021-04-14 17:17:51 +0200

    put all animations in one parent object

--- Assets/00-Project3/Scripts/HandCollision.cs
+++ Assets/00-Project3/Scripts/HandCollision.cs
@@ -4,26 +4,10 @@ using UnityEngine;
 
 public class HandCollision : MonoBehaviour
 {
-    [SerializeField] Animator map;
-    [SerializeField] Animator lid;
-    [SerializeField] AudioSource shimmer;
-    [SerializeField] AudioSource open;
-    [SerializeField] AudioSource close;
+    [SerializeField] Animator treasure;
     private void OnTriggerEnter(Collider collision) 
     {
-        lid = collision.gameObject.GetComponent<Animator>();
-        map.SetBool("lidOpened", !(map.GetBool("lidOpened")));
-        lid.SetBool("touchedLid", !(lid.GetBool("touchedLid")));
-        if(map.GetBool("lidOpened")) 
-        {
-            shimmer.Play();
-            open.Play();
-            close.Pause();
-        }
-        else {
-            shimmer.Pause();
-            close.Play();
-            open.Pause();
-        }
+        treasure = collision.gameObject.GetComponent<Animator>();
+        treasure.SetBool("touchedLid", !(treasure.GetBool("touchedLid")));
     }
 }
