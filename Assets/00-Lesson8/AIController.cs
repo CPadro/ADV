f4c2df7a8811f7541cf5dff89c7717a827f4ebb5
Cristina Padró <34605397+SilverArrow23@users.noreply.github.com>
2021-04-30 19:25:07 +0200

    navmesh

---
+++ Assets/00-Lesson8/AIController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIController : MonoBehaviour
+{
+    public Transform goal;
+    NavMeshAgent agent;
+    Animator animatorController;
+    public Transform mark;
+    public Camera camera;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animatorController = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+            if(Physics.Raycast(ray, out RaycastHit hit))
+            {
+                agent.destination = mark.position = hit.point;
+                //mark.GetComponent<AudioSource>().Play();
+            }
+        }
+        //if(agent.isOnOffMeshLink) { animatorController.SetTrigger("jump"); }
+        animatorController.SetFloat("lateral", transform.InverseTransformDirection(agent.velocity).x);
+        animatorController.SetFloat("advance", transform.InverseTransformDirection(agent.velocity).y);
+    }
+}
