d8ecf3bc5bb6e6d88c4fff196eed2dda54eafa7d
Cristina Padró <34605397+SilverArrow23@users.noreply.github.com>
2021-05-21 20:54:50 +0200

    footsteps work. making diamonds

---
+++ Assets/00-Project4/Scripts/FootSteps.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootSteps : MonoBehaviour
+{
+    [SerializeField] AudioClip clip;
+    private AudioSource source;
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Step()
+    {
+        source.PlayOneShot(clip);
+    }
+}
