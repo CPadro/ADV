2e3999b5e9ee9432ccf95d602e8255a0596d0e3c
Cristina Padró <34605397+SilverArrow23@users.noreply.github.com>
2021-05-21 12:39:19 +0200

    Second timeline triggerd and working properly

---
+++ Assets/00-Project4/Scripts/CinematicCollision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class CinematicCollision : MonoBehaviour
+{
+    [SerializeField] GameObject player;
+    [SerializeField] PlayableDirector cutsceneCam;
+    private void OnTriggerEnter(Collider trigger) 
+    {
+        this.gameObject.GetComponent<BoxCollider>().enabled = false;
+        cutsceneCam.Play();
+        //player.SetActive(false);
+        StartCoroutine(EndCutscene());
+    }
+
+    IEnumerator EndCutscene()
+    {
+        yield return new WaitForSeconds(10);
+        //player.SetActive(true);
+        cutsceneCam.Stop();
+    }
+}
